ba84dbe1cff948cfd18706daad74b201b76acc75
unknown <hello.safrian@gmail.com>
2023-09-04 12:34:39 +0700

    add login page

--- src/layouts/logo/AuthLogo.js
+++ src/layouts/logo/AuthLogo.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/dark-logo-icon.svg';
-import { ReactComponent as LogoDarkText } from '../../assets/images/logos/dark-logo-text.svg';
-import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/white-logo-icon.svg';
-import { ReactComponent as LogoWhiteText } from '../../assets/images/logos/white-logo-text.svg';
+// import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/dark-logo-icon.svg';
+// import { ReactComponent as LogoDarkText } from '../../assets/images/logos/dark-logo-text.svg';
+// import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/white-logo-icon.svg';
+// import { ReactComponent as LogoWhiteText } from '../../assets/images/logos/white-logo-text.svg';
+import Logo from '../../assets/images/logos/pema-logo.png';
 
 const AuthLogo = () => {
   const isDarkMode = useSelector((state) => state.customizer.isDark);
 
   return (
-    <div className="p-4 d-flex justify-content-center gap-2">
+    <div className="p-4 d-flex justify-content-center align-items-end gap-2">
       {isDarkMode !== false ? (
         <>
-          <LogoWhiteIcon />
-          <LogoWhiteText />
+          <img src={Logo} alt="logo" width="40" height="40" />
+          {/* <LogoWhiteIcon />
+          <LogoWhiteText /> */}
         </>
       ) : (
         <>
-          <LogoDarkIcon />
-          <LogoDarkText />
+          <img src={Logo} alt="logo" width="40" height="40" />
+          <h4>PROJECT MANAGEMENT</h4>
+          {/* <LogoDarkIcon /> */}
+          {/* <LogoDarkText /> */}
         </>
       )}
     </div>
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 import Loadable from '../layouts/loader/Loadable';
+import Login from '../pages/auth/Login';
 /****Layouts*****/
 
 const FullLayout = Loadable(lazy(() => import('../layouts/FullLayout')));
@@ -17,7 +18,6 @@ const CASL = Loadable(lazy(() => import('../views/apps/accessControlCASL/AccessC
 /***** Auth Pages ****/
 const Error = Loadable(lazy(() => import('../views/auth/Error')));
 const RegisterFormik = Loadable(lazy(() => import('../views/auth/RegisterFormik')));
-const LoginFormik = Loadable(lazy(() => import('../views/auth/LoginFormik')));
 const Maintanance = Loadable(lazy(() => import('../views/auth/Maintanance')));
 const LockScreen = Loadable(lazy(() => import('../views/auth/LockScreen')));
 const RecoverPassword = Loadable(lazy(() => import('../views/auth/RecoverPassword')));
@@ -43,7 +43,7 @@ const ThemeRoutes = [
       { path: '404', element: <Error /> },
       { path: '*', element: <Navigate to="/auth/404" /> },
       { path: 'registerformik', element: <RegisterFormik /> },
-      { path: 'loginformik', element: <LoginFormik /> },
+      { path: 'login', element: <Login /> },
       { path: 'maintanance', element: <Maintanance /> },
       { path: 'lockscreen', element: <LockScreen /> },
       { path: 'recoverpwd', element: <RecoverPassword /> },
