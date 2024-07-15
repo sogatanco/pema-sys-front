5edbbca78f6266333437e7481041e319cba87d85
sogatanco <adeliapucha46@gmail.com>
2024-07-15 11:55:12 +0700

    ssgsdg;

--- src/pages/asset/ListAsset.js
+++ src/pages/asset/ListAsset.js
@@ -14,6 +14,7 @@ import { alert } from '../../components/atoms/Toast';
 
 const baseURL = process.env.REACT_APP_BASEURL;
 
+// gsdgsdgsta
 const ListAsset = ({ listAsset, refetch , loading1}) => {
   const [search, setSearch] = useState('');
   const [filter, setFilter] = useState([]);
