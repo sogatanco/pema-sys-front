285e4e2ccd5b49b9f9af0192a4616276d4f1478b
pema-dev <mitrapema@gmail.com>
2024-12-23 11:36:48 +0700

    ssss

--- src/pages/adm/index.js
+++ src/pages/adm/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Alert } from 'reactstrap';
 import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
 // import Sppd from './Adm';
 import Adm from './Adm';
 
-const index = () => {
+
+
+const Index = () => {
+
+  const [visible, setVisible] = React.useState(true);
+
+const onDismiss = () => setVisible(false);
+
+ 
   return (
     <>
+     <Alert color="info" isOpen={visible} toggle={onDismiss}>
+      Baca Dokumentasi sistem ADM <a href='https://gofile.me/7mNIn/PBFC3xKHr' target='_blank' rel='noreferrer' style={{textDecoration:'none'}}>disini !!!</a> 
+    </Alert>
       <BreadCrumbs />
       <Adm />
     </>
   );
 };
 
-export default index;
+export default Index;
